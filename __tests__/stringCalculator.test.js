7cdfbd984548dc421bd04bdddc3cfed562ef0a6a
Vivek Kumar <vivekku@emids.com>
2023-05-06 00:33:16 +0530

    added support for calculating sum for single number

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -3,3 +3,7 @@ const stringCalculator = require("../stringCalculator");
 it("should return zero for an empty string", () => {
     expect(stringCalculator("")).toEqual(0);
 });
+
+it("should return the sum equal to the number in case of string contains a single number", () => {
+    expect(stringCalculator("1")).toEqual(1);
+});
