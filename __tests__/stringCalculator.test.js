8e2e2af1c9f559771b957075f34d8f38beaf1ad0
Vivek Kumar <vivekku@emids.com>
2023-05-06 00:34:52 +0530

    added test case for more than one number in the string

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -7,3 +7,7 @@ it("should return zero for an empty string", () => {
 it("should return the sum equal to the number in case of string contains a single number", () => {
     expect(stringCalculator("1")).toEqual(1);
 });
+
+it("should return sum for the numbers seperated with comma", () => {
+    expect(stringCalculator("1,2,3")).toEqual(6);
+});
